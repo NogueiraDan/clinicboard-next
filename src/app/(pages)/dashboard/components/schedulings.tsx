87f03162245ca173e01107fca3647193b7a8a9cf
NogueiraDan <silvanogueira3460@gmail.com>
2024-10-19 18:23:57 -0300

    feat: atualiza componente Schedulings

--- src/app/(pages)/dashboard/components/schedulings.tsx
+++ src/app/(pages)/dashboard/components/schedulings.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { Calendar } from "@/components/ui/calendar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
 
 export default function Schedulings() {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
     <Card className="col-span-3">
       <CardHeader>
@@ -10,13 +14,15 @@ export default function Schedulings() {
       <CardContent className="pl-2 flex justify-between">
         <Calendar
           mode="single"
+          selected={date}
+          onSelect={setDate}
           className="rounded-md border w-[300px] flex items-center justify-center"
         />
         <div className="w-[60%] max-h-64 overflow-y-auto">
-          <p className="rounded border h-[116px] w-full mb-3">Schedulings</p>
-          <p className="rounded border h-[116px] w-full mb-3">Schedulings</p>
-          <p className="rounded border h-[116px] w-full mb-3">Schedulings</p>
-          <p className="rounded border h-[116px] w-full mb-3">Schedulings</p>
+          <p className="rounded border h-[116px] w-full mb-3">Scheduling: {date?.toLocaleDateString()}</p>
+          <p className="rounded border h-[116px] w-full mb-3">Scheduling: {date?.toLocaleDateString()}</p>
+          <p className="rounded border h-[116px] w-full mb-3">Scheduling: {date?.toLocaleDateString()}</p>
+          <p className="rounded border h-[116px] w-full mb-3">Scheduling: {date?.toLocaleDateString()}</p>
         </div>
       </CardContent>
     </Card>
