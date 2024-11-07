92034206292e009134b9e920cf3134a3667aa2d7
NogueiraDan <silvanogueira3460@gmail.com>
2024-11-07 20:52:49 -0300

    feat: adiciona diálogo de confirmação para exclusão de agendamentos e implementa hook para deletar agendamentos

--- src/app/(pages)/dashboard/components/schedule-details.tsx
+++ src/app/(pages)/dashboard/components/schedule-details.tsx
@@ -10,6 +10,15 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { ScheduleDetailsProps } from "../types";
 
@@ -25,15 +34,40 @@ export default function ScheduleDetails({ patientId }: ScheduleDetailsProps) {
           <AlertDialogTitle>Detalhes da consulta</AlertDialogTitle>
           <AlertDialogDescription>
             <div className="flex flex-col gap-2">
-                <p className="text-base font-medium text-slate-700">Paciente: {patient.name}</p>
-                <p className="text-base font-medium text-slate-700">Email: {patient.email}</p>
-                <p className="text-base font-medium text-slate-700">Contato: {patient.phone}</p>
-                <p className="text-base font-medium text-slate-700">Endereço: {patient.street}, {patient.district}, {patient.city}, {patient.state}</p>
+              <p className="text-base font-medium text-slate-700">
+                Paciente: {patient.name}
+              </p>
+              <p className="text-base font-medium text-slate-700">
+                Email: {patient.email}
+              </p>
+              <p className="text-base font-medium text-slate-700">
+                Contato: {patient.phone}
+              </p>
+              <p className="text-base font-medium text-slate-700">
+                Endereço: {patient.street}, {patient.district}, {patient.city},{" "}
+                {patient.state}
+              </p>
             </div>
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
-          {/* <AlertDialogCancel>Cancel</AlertDialogCancel> */}
+          <Dialog>
+            <DialogTrigger>
+              <Button variant="destructive">Excluir</Button>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>Tem certeza disso?</DialogTitle>
+                <DialogDescription>
+                  Essa ação é irreversível e excluirá o agendamento do paciente
+                </DialogDescription>
+              </DialogHeader>
+              <DialogFooter>
+                <Button variant="destructive">Sim, quero excluir</Button>
+              </DialogFooter>
+            </DialogContent>
+          </Dialog>
+
           <AlertDialogAction>Fechar</AlertDialogAction>
         </AlertDialogFooter>
       </AlertDialogContent>
---
+++ src/app/hooks/useDeleteSchedule.ts
@@ -0,0 +1,28 @@
+import { useMutation } from "@tanstack/react-query";
+import api from "../service/api";
+import { toast } from "react-toastify";
+import { isAxiosError } from "axios";
+
+export function useDeleteSchedule() {
+    const { mutateAsync } = useMutation({
+      mutationFn: async (id: string) => {
+        await api.delete(`/scheduling/${id}`);
+      },
+      onSuccess: () => {
+        toast.success("Agendamento deletado com sucesso!");
+        window.location.reload();
+      },
+      onError(error) {
+        console.log("Caiu no onError do useDeleteSchedule", error);
+        if (isAxiosError(error)) {
+          toast.error(error.response?.data.message);
+          console.log(error);
+        }
+      },
+    });
+  
+    return {
+      deleteSchedule: mutateAsync,
+    };
+  }
+  
