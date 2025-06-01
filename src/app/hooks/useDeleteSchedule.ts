dc984cdd64abeb193185904c2469b956abda0061
NogueiraDan <silvanogueira3460@gmail.com>
2025-06-01 14:40:28 -0300

    fix: corrigir endpoint de deleção de agendamento e adicionar cabeçalhos à requisição

--- src/app/hooks/useDeleteSchedule.ts
+++ src/app/hooks/useDeleteSchedule.ts
@@ -2,11 +2,14 @@ import { useMutation } from "@tanstack/react-query";
 import api from "../service/api";
 import { toast } from "react-toastify";
 import { isAxiosError } from "axios";
+import { fetchHeaders } from "../utils/fetch-headers";
 
 export function useDeleteSchedule(id: string) {
     const { mutateAsync } = useMutation({
       mutationFn: async () => {
-        await api.delete(`/scheduling/${id}`);
+        await api.delete(`/appointments/${id}`,{
+          headers: fetchHeaders(),
+        });
       },
       onSuccess: () => {
         toast.success("Agendamento deletado com sucesso!");
